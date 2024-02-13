db228d5c1f91670dadece1e3a04ee2e06e0f97b9
John Cox <john.cox0321@gmail.com>
2024-02-13 15:27:36 -0600

    added a period

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
 	return (
 		<div className="p-12 flex items-center">
 			<h1 className="text-secondaryText font-semibold text-4xl">
-				Welcome Back, Let's Get to Practice
+				Welcome Back, Let's Get to Practice.
 			</h1>
 		</div>
 	);
